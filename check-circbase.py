dc12199dc1a3febfa6bc0af26b7fe3f135776dc9
LGray95 <lachlan.gray95@gmail.com>
2020-04-29 14:54:16 +1000

    Add arguments

--- check-circbase.py
+++ check-circbase.py
@@ -1,30 +1,45 @@
 from split_coordinates import split_coords
+import argparse
 
-circfile =  open("/Users/lachlan/Downloads/infile.txt", "r").read().splitlines()
-circBase =  open('/Users/lachlan/OneDrive - UNSW/Masters_Research_2019/Data/circBase/hsa_hg19_circRNA.txt', "r")
 
-circ_list = []
-circBase_list = []
-for entry in circfile:
-    circRNA_coords = entry
-    circ_chr = split_coords(circRNA_coords)[0]
-    circ_start = int(split_coords(circRNA_coords)[1])
-    circ_end = int(split_coords(circRNA_coords)[2])
+def argparser():
+    parser = argparse.ArgumentParser(description='Ask for path')
+    parser.add_argument("-c", "--coords", help = "circRNA coordinates")
+    parser.add_argument("-i", "--infile", help = "Path to circBase file")
+    args = parser.parse_args()
+    return args
 
-    list_a = circ_chr, circ_start, circ_end
-    circ_list.append(list_a)
 
-for line in circBase:
-    # print(line)
-    col = line.split("\t")
-    if "#" not in line:
-        chr = col[0]
-        start = int(col[1])
-        end = int(col[2])
-        list_b = chr, start, end
-        circBase_list.append(list_b)
+def read_files(args):
+    circBase = open(args.infile, "r").read().splitlines()
+    return circBase
 
-for i in circ_list:
-    for j in circBase_list:
-        if i[0] == j[0] and (i[1] - j[1]) != 0:
-            print(line)
+
+def check_circBase(args, circBase):
+    match = []
+    for line in circBase:
+        col = line.split("\t")
+        if "#" not in line:
+            if split_coords(args.coords)[0] == col[0] and split_coords(args.coords)[1] == int(col[1]) and split_coords(args.coords)[2] == int(col[2]):
+                match.append(line)
+    return match
+
+
+def main():
+
+    # Set command line arguments
+    args = argparser()
+
+    # Read in circBase file
+    circBase = read_files(args)
+
+    # Perform match algorithm
+    match = check_circBase(args, circBase)
+
+    # Print all matches in file
+    for element in match:
+        print(element)
+
+
+if __name__ == '__main__':
+    main()
